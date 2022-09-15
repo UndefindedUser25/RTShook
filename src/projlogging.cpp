ecbf7a69a9134f05a1531a429a1086fc5eb03616
sibura7724 <108341137+sibura7724@users.noreply.github.com>
2022-09-15 11:18:18 +0700

    Update projlogging.cpp

--- src/projlogging.cpp
+++ src/projlogging.cpp
@@ -14,11 +14,8 @@ Vector prevloc[2048]{};
 
 void Update()
 {
-    for (int i = 1; i <= HIGHEST_ENTITY; i++)
+    for (auto &ent : entity_cache::valid_ents)
     {
-        CachedEntity *ent = ENTITY(i);
-        if (CE_BAD(ent))
-            continue;
         const model_t *model = RAW_ENT(ent)->GetModel();
         bool issandwich      = false;
         if (model && tickcount % 33 == 0)
@@ -27,10 +24,11 @@ void Update()
             if (model_name.find("plate") != std::string::npos)
             {
                 issandwich      = true;
+                const uint16_t curr_idx = ent->m_IDX;
                 Vector abs_orig = RAW_ENT(ent)->GetAbsOrigin();
-                float movement  = prevloc[i].DistTo(abs_orig);
+                float movement  = prevloc[curr_idx].DistTo(abs_orig);
                 logging::Info("Sandwich movement: %f", movement);
-                prevloc[i] = abs_orig;
+                prevloc[curr_idx] = abs_orig;
             }
         }
         if (ent->m_Type() == ENTITY_PROJECTILE || issandwich)
@@ -41,9 +39,9 @@ void Update()
             if (tickcount % 20 == 0)
             {
                 Vector abs_orig = RAW_ENT(ent)->GetAbsOrigin();
-                float movement  = prevloc[i].DistTo(abs_orig);
+                float movement  = prevloc[curr_idx].DistTo(abs_orig);
                 logging::Info("movement: %f", movement);
-                prevloc[i]      = abs_orig;
+                prevloc[curr_idx]      = abs_orig;
                 const Vector &v = ent->m_vecVelocity;
                 Vector eav;
                 velocity::EstimateAbsVelocity(RAW_ENT(ent), eav);
@@ -54,7 +52,7 @@ void Update()
                 // v.Length(), a.x, a.y, a.z);
                 logging::Info("%d [%s]: CatVelocity: %.2f %.2f %.2f (%.2f) | "
                               "EAV: %.2f %.2f %.2f (%.2f)",
-                              i, RAW_ENT(ent)->GetClientClass()->GetName(), v.x, v.y, v.z, v.Length(), eav.x, eav.y, eav.z, eav.Length());
+                              curr_idx, RAW_ENT(ent)->GetClientClass()->GetName(), v.x, v.y, v.z, v.Length(), eav.x, eav.y, eav.z, eav.Length());
             }
         }
     }

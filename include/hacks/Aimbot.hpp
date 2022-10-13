042fb1c64ec2fa83cd37071c12a94bb924f16c19
sibura7724 <xdsuper432@gmail.com>
2022-10-14 05:14:36 +0700

    rewhite aimbot

--- include/hacks/Aimbot.hpp
+++ include/hacks/Aimbot.hpp
@@ -1,5 +1,5 @@
 /*
- * HAimbot.h
+ * Aimbot.hpp
  *
  *  Created on: Oct 8, 2016
  *      Author: nullifiedcat
@@ -14,13 +14,14 @@ class IClientEntity;
 
 namespace hacks::aimbot
 {
+extern settings::Boolean aim_sentrybuster;
 extern settings::Boolean ignore_cloak;
 extern unsigned last_target_ignore_timer;
 // Used to store aimbot data to prevent calculating it again
 struct AimbotCalculatedData_s
 {
     unsigned long predict_tick{ 0 };
-    bool predict_type{ 0 };
+    bool predict_type{ false };
     Vector aim_position{ 0 };
     unsigned long vcheck_tick{ 0 };
     bool visible{ false };
@@ -29,25 +30,27 @@ struct AimbotCalculatedData_s
 };
 
 // Functions used to calculate aimbot data, and if already calculated use it
-Vector PredictEntity(CachedEntity *entity, bool vischeck);
-bool VischeckPredictedEntity(CachedEntity *entity);
+Vector PredictEntity(CachedEntity *entity);
 bool BacktrackVisCheck(CachedEntity *entity);
 
-// Functions called by other functions for when certian game calls are run
+// Functions called by other functions for when certain game calls are run
 void Reset();
 
 // Stuff to make storing functions easy
-void doAutoZoom(bool target_found);
 bool isAiming();
 CachedEntity *CurrentTarget();
 bool ShouldAim();
 CachedEntity *RetrieveBestTarget(bool aimkey_state);
 bool IsTargetStateGood(CachedEntity *entity);
-void Aim(CachedEntity *entity);
+bool Aim(CachedEntity *entity);
 void DoAutoshoot(CachedEntity *target = nullptr);
-int BestHitbox(CachedEntity *target);
+int notVisibleHitbox(CachedEntity *target, int preferred);
 std::vector<Vector> getHitpointsVischeck(CachedEntity *ent, int hitbox);
 float projectileHitboxSize(int projectile_size);
+int autoHitbox(CachedEntity *target);
+bool hitscanSpecialCases(CachedEntity *target_entity, int weapon_case);
+bool projectileSpecialCases(CachedEntity *target_entity, int weapon_case);
+int BestHitbox(CachedEntity *target);
 bool isHitboxMedium(int hitbox);
 int ClosestHitbox(CachedEntity *target);
 void DoSlowAim(Vector &inputAngle);
